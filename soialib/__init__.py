9821e6fad5ffd6a4fd1254a0e504a756f89cbcbb
Tyler Fibonacci <gepheum@gmail.com>
2024-10-24 16:21:22 -0400

    fix imports

--- soialib/__init__.py
+++ soialib/__init__.py
@@ -1,5 +1,5 @@
-from keyed_items import KeyedItems
-from serializer import Serializer
-from timestamp import Timestamp
+from soialib.keyed_items import KeyedItems
+from soialib.serializer import Serializer
+from soialib.timestamp import Timestamp
 
 __all__ = ["KeyedItems", "Serializer", "Timestamp"]
